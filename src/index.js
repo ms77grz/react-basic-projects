a183ce5e838a6caf9191900cb9fbe4167e4ebfb9
Magomed Shamaev <ms77grz@pop-os.localdomain>
2022-05-15 22:57:01 +0300

    Markdown Preview

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import your current project here
-// import App from './projects/14-cart/App';
-// import { AppProvider } from './projects/14-cart/context';
 
-import App from './projects/15-cocktails/App';
-import { AppProvider } from './projects/15-cocktails/context';
+// import your current project here
+import App from './projects/16-markdown-preview/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <App />
   </React.StrictMode>
 );
---
+++ src/projects/16-markdown-preview/App.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './App.css';
+
+function App() {
+  const [markdown, setMarkdown] = useState('## markdown preview');
+
+  return (
+    <main>
+      <section className='markdown'>
+        <textarea
+          className='input'
+          value={markdown}
+          onChange={e => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className='result'>
+          <ReactMarkdown children={markdown} />
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default App;
