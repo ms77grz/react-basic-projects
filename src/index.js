d1cdcdae91da3403625a4903287045fe8ba1d017
Magomed Shamaev <ms77grz@pop-os.localdomain>
2022-05-18 23:26:13 +0300

    Random User

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // import your current project here
-import App from './projects/16-markdown-preview/App';
+import App from './projects/17-random-person/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
